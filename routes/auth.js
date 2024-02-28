41d8807a1fe92649f696e698ea03aa90b7626fec
HuuNgo12 <ngovanhuu1602@gmail.com>
2024-02-28 14:07:42 +0700

    Server_first_commit

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 //middleware
+
 const { authCheck } = require("../middlewares/auth");
 //controler
 const { createOrUpdateUser } = require("../controllers/auth");
 
-router.post("/create-or-update-user", authCheck, createOrUpdateUser); // khi dòng code này chạy thì nó sẽ thực hiện tuần tự các function
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+// khi dòng code này chạy thì nó sẽ thực hiện tuần tự các function
 
 module.exports = router;
