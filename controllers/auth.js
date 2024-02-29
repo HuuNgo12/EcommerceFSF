17ed5a90efa32d239d161813ae494fe8ddc230f6
HuuNgo12 <ngovanhuu1602@gmail.com>
2024-02-29 09:05:46 +0700

    API User Response (middleware: verify the user token send from client. Controller: create the information in order to send back to client)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,28 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: "hey you hit  create-or-update-user API endpoint",
-  });
+const User = require("../models/user");
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    // đây là 1 phương thức của mongleDB: tìm đối tượng user theo email: nếu
+    // tồn tại thì update name và picture. CÒn nếu k tồn tại thì user là null
+    { email },
+    { name: email.split("@")[0], picture },
+    { new: true }
+  );
+
+  if (user) {
+    console.log("USER UPDATED", user);
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name: email.split("@")[0],
+      picture,
+    }).save();
+    console.log("USER CREATED", newUser);
+
+    res.json(newUser);
+  }
+  // vòng If này là check nếu user đã tồn tại thì res, còn nếu chưa tồn tại thì tạo mới 1 user với email, name, picture
 };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,15 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers); //token
-  next(); // this is a callback function. This  function is called when we want to move to next middleware.
+exports.authCheck = async (req, res, next) => {
+  // console.log(req.headers); //token
+  try {
+    const firebaseUser = await admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    // console.log("FIREBAE USER IN AUTHCHECK", firebaseUser);
+    req.user = firebaseUser;
+    next(); // this is a callback function. This  function is called when we want to move to next middleware.
+  } catch (err) {
+    res.status(401).json({ err: "Invalid or expired token" });
+  }
 };
