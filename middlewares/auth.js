bed7869e18fb37891f204e692628532a22c09aea
HuuNgo12 <ngovanhuu1602@gmail.com>
2024-03-04 14:08:50 +0700

    Add Admin router, Middleware check role admin.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require("../firebase");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
   // console.log(req.headers); //token
@@ -11,5 +12,23 @@ exports.authCheck = async (req, res, next) => {
     next(); // this is a callback function. This  function is called when we want to move to next middleware.
   } catch (err) {
     res.status(401).json({ err: "Invalid or expired token" });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  // const { email } = req.user;
+  try {
+    const adminUser = await User.findOne({ email: req.user.email }).exec();
+    if (adminUser.role !== "admin") {
+      return res.status(403).json({
+        err: "Admin resource. Access denied",
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    // Xử lý lỗi nếu có
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 //middleware
 
-const { authCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require("../middlewares/auth");
 //controler
 const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 router.post("/current-user", authCheck, currentUser);
+router.post("/current-admin", authCheck, adminCheck, currentUser);
 // khi dòng code này chạy thì nó sẽ thực hiện tuần tự các function
 
 module.exports = router;
